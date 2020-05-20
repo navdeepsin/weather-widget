e3a530a4b18f071e9b8640bceb127257d66354ae
Navdeepsin <navdeepsin31@gmail.com>
2020-05-20 15:08:00 -0500

    connects with the open weather map api

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const apiKey = "4f4ae9f965cf307b8658cdd4a0458dba";
+
 if(navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position){
     let lat = position.coords.latitude;
-    let long = position.coords.longitude;
-    console.log(lat, long);
+    let lon = position.coords.longitude;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+      .then (resp => {
+        if (resp.ok) {
+          return(resp.json());
+        }
+      }).then(data => {
+        console.log(data);
+      })  
   })
 } else {
   console.log("Geolocation is not supported");
