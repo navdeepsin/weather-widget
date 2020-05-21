59f6898eba2f8748acb26645837f8ecafb59913c
Navdeepsin <navdeepsin31@gmail.com>
2020-05-20 19:08:33 -0500

    prints the 5 days icon, maximum temperature and lowest temperature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const apiKey = "4f4ae9f965cf307b8658cdd4a0458dba";
 const currentConditions = document.querySelector('.current-conditions');
 const forecast = document.querySelector('.forecast');
 
+
 if(navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position){
     let lat = position.coords.latitude;
@@ -43,7 +44,8 @@ function insertCurrentWeather(data) {
 
 function insertForecast(data) {
   for(i = 0; i < data.cnt; i += 8) {
-    forecast.insertAdjacentHTML('afterbegin', `
+    
+    forecast.insertAdjacentHTML('beforeend', `
     <div class="day">
     <h3>Tuesday</h3>
     <img src="http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" />
